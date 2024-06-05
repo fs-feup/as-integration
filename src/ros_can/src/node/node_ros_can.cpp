641903a6295b91135037e25afa73941de0f6af75
FSFEUP <formulastudentfeup@gmail.com>
2024-06-05 21:31:49 +0100

    ref(ros-can): added print to failure opening can channel

--- src/ros_can/src/node/node_ros_can.cpp
+++ src/ros_can/src/node/node_ros_can.cpp
@@ -46,9 +46,12 @@ RosCan::RosCan(std::shared_ptr<ICanLibWrapper> can_lib_wrapper_param)
   // initialize the CAN library
   canInitializeLibrary();
   // A channel to a CAN circuit is opened. The channel depend on the hardware
-  hnd_ = canOpenChannel(0, canOPEN_EXCLUSIVE);
+  hnd_ = canOpenChannel(0, canOPEN_CAN_FD);
+  if (hnd_ < 0) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to open CAN channel. Handle value: %d", hnd_);
+  }
   // Setup CAN parameters for the channel
-  stat_ = canSetBusParams(hnd_, canBITRATE_500K, 0, 0, 4, 0, 0);  // check these values later
+  stat_ = canSetBusParams(hnd_, canBITRATE_500K, 0, 0, 1, 0, 0);  // check these values later
   if (stat_ != canOK) {
     RCLCPP_ERROR(this->get_logger(), "Failed to setup CAN parameters");
   }
